23b6fd0684e2f6ec1b1e5cc7da1e62160679cfb4
Umad Akram <152366259+AkramUmad572@users.noreply.github.com>
2026-09-06 23:03:57 -0400

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        visited, counter = set(), 0
+
+        for a,b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        
+        def dfs(node):
+            visited.add(node)
+            for nei in adj[node]:
+                if nei not in visited:
+                    dfs(nei)
+        
+        for node in range(n):
+            if node in visited:
+                pass
+            elif node not in visited:
+                counter += 1
+                dfs(node)
+        return counter
